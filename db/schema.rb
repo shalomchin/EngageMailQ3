4bfe240139661f8b788b53e976f43862a75efb21
Unknown <stanshalomchin@yahoo.com>
2017-05-09 18:38:07 +0800

    Scaffold Admin Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509050536) do
+ActiveRecord::Schema.define(version: 20170509060625) do
+
+  create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "surveys", force: :cascade do |t|
     t.integer  "answer1"
