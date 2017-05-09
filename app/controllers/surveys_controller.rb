e67b5a54ee89903b2c4698688a2af649ace09209
Unknown <stanshalomchin@yahoo.com>
2017-05-09 12:20:05 +0800

    Implement user on website

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,6 +1,6 @@
 class SurveysController < ApplicationController
   before_action :set_survey, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!
   # GET /surveys
   # GET /surveys.json
   def index
